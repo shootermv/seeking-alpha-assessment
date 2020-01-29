a27dca5a77887d91c9930f63dad2be7180818ac9
moshev <moshev@welldone-software.com>
2020-01-29 13:30:23 +0200

    Updates

---
+++ precache-manifest.98f66b182736deac14bbd2a800fe74ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2250f489a12581585081d3b46f8b23",
+    "url": "/seeking-alpha-assessment/index.html"
+  },
+  {
+    "revision": "3015976332558933bd04",
+    "url": "/seeking-alpha-assessment/static/css/main.e75f6a57.chunk.css"
+  },
+  {
+    "revision": "2173aa7451f4e78518a3",
+    "url": "/seeking-alpha-assessment/static/js/2.f9cf05bf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/seeking-alpha-assessment/static/js/2.f9cf05bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3015976332558933bd04",
+    "url": "/seeking-alpha-assessment/static/js/main.4bef4b2b.chunk.js"
+  },
+  {
+    "revision": "852be44ef8f91b83f045",
+    "url": "/seeking-alpha-assessment/static/js/runtime-main.dfb10f37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/seeking-alpha-assessment/static/media/logo.5d5d9eef.svg"
+  }
+]);
